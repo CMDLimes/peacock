2d1a2e1d63a27532397b533b41888b5f1f0cbf25
Cheukting <cheukting.ho@gmail.com>
2024-09-07 23:11:18 +0100

    adding fields in membership model

--- peacock/membership/models.py
+++ peacock/membership/models.py
@@ -7,7 +7,7 @@
 # avatar: (optional)
 # pronouns (optional)
 # location (?)
-# List of projects. (optional for now)
+# List of projects. (optional for now) **imprement later**
 # GitHub handle: connect with GitHub API.
 # confirmed status: whether email address is confirmed
 # approval status: whether approved by us. (True false)
@@ -16,4 +16,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Member(AbstractUser):
-    twitter_url = models.CharField(max_length=100)
+    twitter_url = models.CharField(max_length=100, null=True, blank=True)
+    linkedin_url = models.CharField(max_length=100, null=True, blank=True)
+    mastodon_url = models.CharField(max_length=100, null=True, blank=True)
+    bio = models.CharField(max_length=1000, null=True, blank=True)
+    avatar = models.ImageField(upload_to='images/', null=True, blank=True)
+    pronouns = models.CharField(max_length=40, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    github_handle = models.CharField(max_length=40)
+    confirmed_status = models.BooleanField(default = False)
+    approved_status = models.BooleanField(default = False)
+    approved_by = models.CharField(max_length=40, null=True, blank=True)
