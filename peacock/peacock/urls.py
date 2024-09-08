23589deafce75f5a57c820cc68f1c1ca45b35654
Cheukting <cheukting.ho@gmail.com>
2024-09-08 23:44:45 +0100

    start working on a theme

--- peacock/peacock/urls.py
+++ peacock/peacock/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("membership/", include("membership.urls")),
+    path('', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+  # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
