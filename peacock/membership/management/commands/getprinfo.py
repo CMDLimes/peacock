9784c95bf0c84c0a3709f71bd4c20cc025aede53
Cheukting <cheukting.ho@gmail.com>
2024-09-10 23:20:58 +0100

    add pagnination handeling to get all results

--- peacock/membership/management/commands/getprinfo.py
+++ peacock/membership/management/commands/getprinfo.py
@@ -19,30 +19,38 @@ class Command(BaseCommand):
         headers = {"Authorization": os.environ["GH_TOKEN"]}
 
         for member in Member.objects.all():
-            url = f'https://api.github.com/search/issues?q=type:pr+author:{member.github_handle}'
-            res = requests.get(url, headers=headers)
-            prs_json = json.loads(res.text)["items"]
-            for pr in prs_json:
-                repo = "/".join(pr["repository_url"].split("/")[-2:])
-                id = "/".join(pr["html_url"].split("/")[-4:])
-                if repo in repos:
-                    pr_obj = list(Contribution.objects.filter(gh_id=pr["id"]))
-                    if len(pr_obj) != 0:
-                        obj = pr_obj[0]
-                        obj.title = pr["title"]
-                        obj.state = pr["state"]
-                        obj.merged_at = pr.get("merged_at", None)
-                        obj.save()
-                    else:
-                        obj = Contribution.objects.create(
-                            id = id,
-                            gh_id = pr["id"],
-                            author = member,
-                            html_url = pr["html_url"],
-                            repo = Project.objects.get(full_name=repo),
-                            title = pr["title"],
-                            state = pr["state"],
-                            created_at = pr["created_at"],
-                            merged_at = pr.get("merged_at", None)
-                        )
-                        obj.save()
+            url = f'https://api.github.com/search/issues?q=type:pr+author:{member.github_handle}&per_page=100'
+            while url is not None:
+                res = requests.get(url, headers=headers)
+                # set url to the next page, or None is no next page
+                url = None
+                for link in res.headers["Link"].split(","):
+                    parts = link.strip().split(";")
+                    if parts[1].strip() == 'rel="next"':
+                        url = parts[0].strip("<").strip(">")
+                # parse and handle results
+                prs_json = json.loads(res.text)["items"]
+                for pr in prs_json:
+                    repo = "/".join(pr["repository_url"].split("/")[-2:])
+                    id = "/".join(pr["html_url"].split("/")[-4:])
+                    if repo in repos:
+                        pr_obj = list(Contribution.objects.filter(gh_id=pr["id"]))
+                        if len(pr_obj) != 0:
+                            obj = pr_obj[0]
+                            obj.title = pr["title"]
+                            obj.state = pr["state"]
+                            obj.merged_at = pr.get("merged_at", None)
+                            obj.save()
+                        else:
+                            obj = Contribution.objects.create(
+                                id = id,
+                                gh_id = pr["id"],
+                                author = member,
+                                html_url = pr["html_url"],
+                                repo = Project.objects.get(full_name=repo),
+                                title = pr["title"],
+                                state = pr["state"],
+                                created_at = pr["created_at"],
+                                merged_at = pr.get("merged_at", None)
+                            )
+                            obj.save()
